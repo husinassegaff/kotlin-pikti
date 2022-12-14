3244d5451c0017016d1d982d56ae9ca313b34103
Ahdan Amanullah <ahdanamanullah@gmail.com>
2022-12-14 17:26:29 +0700

    chore: common, adapter, retrofit, dependencies

---
+++ app/src/main/java/com/example/learn_kotlin/common/Common.kt
@@ -0,0 +1,16 @@
+package com.example.learn_kotlin.common
+
+import android.location.Location
+import java.text.SimpleDateFormat
+import java.util.*
+
+class Common {
+    val APP_ID = "4ca775042db05cedbbe80895b18afb62"
+    var current_location: Location? = null
+
+    fun convertUnixToDate(dt: Int): String? {
+        val date = Date(dt * 1000L)
+        val sdf = SimpleDateFormat("HH:mm EEE MM yyyy")
+        return sdf.format(date)
+    }
+}
---
+++ app/src/main/java/com/example/learn_kotlin/retrofit/RetrofitClient.kt
@@ -0,0 +1,18 @@
+package com.example.learn_kotlin.retrofit
+
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+
+class RetrofitClient {
+    private fun loadData(): Retrofit {
+        val instance = Retrofit.Builder()
+            .baseUrl("https://api.openweathermap.org/data/2.5/")
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .client(OkHttpClient())
+            .build()
+        return instance
+    }
+}
