0b70a1d599db91878b9366667cff645c7d28df06
husinassegaff <husinassegaff15@gmail.com>
2022-12-14 20:25:00 +0700

    feat: update TodayWeatherFragment.kt

--- app/src/main/java/com/example/learn_kotlin/TodayWeatherFragment.kt
+++ app/src/main/java/com/example/learn_kotlin/TodayWeatherFragment.kt
@@ -5,10 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.learn_kotlin.common.Common
 import com.example.learn_kotlin.databinding.FragmentTodayWeatherBinding
 import com.example.learn_kotlin.retrofit.IOpenWeatherMap
 import com.example.learn_kotlin.retrofit.RetrofitClient
+import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
 import retrofit2.Retrofit
 
 class TodayWeatherFragment : Fragment() {
@@ -29,12 +32,37 @@ class TodayWeatherFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        // inisialisasi compositedisposable
         compositeDisposable = CompositeDisposable()
         retrofit = RetrofitClient.loadData()
         mService = retrofit.create(IOpenWeatherMap::class.java)
 
+        getWeatherInformation()
     }
+
+    private fun getWeatherInformation() {
+        compositeDisposable.add(mService.getWeatherByLatLng(Common.current_location?.latitude.toString(), Common.current_location?.longitude.toString(), Common.APP_ID, "metric")
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe() { weatherResult ->
+
+                binding.cityName.text = weatherResult.name
+                binding.txtDescription.text = StringBuilder("Weather in ${weatherResult.name}")
+                binding.txtTemperature.text = StringBuilder(weatherResult.main?.temp.toString()).append("°C").toString()
+                binding.txtDateTime.text = weatherResult.dt?.let { Common.convertUnixToDate(it) }
+                binding.txtWind.text = StringBuilder(weatherResult.wind?.speed.toString()).append("km/h").toString()
+                binding.txtPressure.text = StringBuilder("${weatherResult.main?.pressure} hpa").toString()
+                binding.txtHumidity.text = StringBuilder("${weatherResult.main?.humidity} %").toString()
+                binding.txtSunrise.text = weatherResult.sys?.sunrise?.let {
+                    Common.convertUnixToHour(
+                        it
+                    )
+                }
+                binding.txtSunset.text = weatherResult.sys?.sunset?.let { Common.convertUnixToHour(it) }
+                binding.txtGeoCoordinate.text = StringBuilder("[${weatherResult.coord?.lat}, ${weatherResult.coord?.lon}]").toString()
+            })
+    }
+
+
 
 
 }
--- app/src/main/java/com/example/learn_kotlin/common/Common.kt
+++ app/src/main/java/com/example/learn_kotlin/common/Common.kt
@@ -5,19 +5,24 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class Common {
-    val APP_ID = "4ca775042db05cedbbe80895b18afb62"
-    var current_location: Location? = null
 
-    fun convertUnixToDate(dt: Int): String? {
-        val date = Date(dt * 1000L)
-        val sdf = SimpleDateFormat("HH:mm")
-        return sdf.format(date)
+    companion object {
+        val APP_ID = "4ca775042db05cedbbe80895b18afb62"
+        var current_location: Location? = null
+
+        fun convertUnixToDate(dt: Int): String? {
+            val date = Date(dt * 1000L)
+            val sdf = SimpleDateFormat("HH:mm")
+            return sdf.format(date)
+        }
+
+        fun convertUnixToHour(dt: Int): String? {
+            val date = Date(dt * 1000L)
+            val sdf = SimpleDateFormat("HH:mm EEE MM yyyy")
+            return sdf.format(date)
+        }
     }
 
-    fun convertUnixToHour(dt: Int): String? {
-        val date = Date(dt * 1000L)
-        val sdf = SimpleDateFormat("HH:mm EEE MM yyyy")
-        return sdf.format(date)
-    }
+
 
 }
