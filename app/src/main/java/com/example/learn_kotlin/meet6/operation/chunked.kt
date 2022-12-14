7de9762c15cef8f583939ce86135fe02eade175a
husinassegaff <husinassegaff15@gmail.com>
2022-12-14 16:10:02 +0700

    feat: delete package & code about learn-kotlin

--- app/src/main/java/com/example/learn_kotlin/meet6/operation/chunked.kt
+++
@@ -1,8 +0,0 @@
-package com.example.learn_kotlin.meet6.operation
-
-fun main() {
-    val word = "Institut Teknologi Sepuluh Nopember"
-    val chunked = word.chunked(3)
-
-    println(chunked)
-}
--- app/src/main/java/com/example/learn_kotlin/meet6/operation/distinct.kt
+++
@@ -1,8 +0,0 @@
-package com.example.learn_kotlin.meet6.operation
-
-fun main() {
-    val total = listOf(1, 2, 1, 3, 4, 5, 2, 3, 4, 5)
-    val distinct = total.distinct()
-
-    println(distinct)
-}
