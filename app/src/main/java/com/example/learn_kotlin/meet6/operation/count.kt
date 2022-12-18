01d403bf253a4e05ec2358088f5e2c468a1d96f0
Ahdan Amanullah <ahdanamanullah@gmail.com>
2022-12-19 05:38:48 +0700

    init ptoject notes-app

--- app/src/main/java/com/example/learn_kotlin/meet6/operation/count.kt
+++
@@ -1,7 +0,0 @@
-package com.example.learn_kotlin.meet6.operation
-
-fun main() {
-    val numberList = listOf(1, 2, 3, 4, 5, 6, 7, 8, 9, 10)
-    println(numberList.count())
-    println(numberList.count { it % 3 == 0 })
-}
--- app/src/main/java/com/example/learn_kotlin/meet6/operation/sorted.kt
+++
@@ -1,9 +0,0 @@
-package com.example.learn_kotlin.meet6.operation
-
-fun main() {
-    val kotlinChar = listOf("Husin", "Hisin", "Hasin", "Hosin", "Hesin")
-    val ascendingSort = kotlinChar.sorted()
-    val descendingSort = kotlinChar.sortedDescending()
-    println(ascendingSort)
-    println(descendingSort)
-}
